ee4d06ac28e05d69ffe83100eefe93edc1b6ab0b
Gaston Rider <joshuaadams18@gmail.com>
2017-04-27 20:40:55 -0400

    Initial Commit

---
+++ Assets/Camera & UI/Cursor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+
+    CameraRaycaster cameraRayCaster;
+
+	// Use this for initialization
+	void Start () {
+        cameraRayCaster = GetComponent<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(cameraRayCaster.layerHit);
+	}
+}
