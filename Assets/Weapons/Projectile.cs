104bae87352c3986addd6f14e36c363a8309965d
Gaston Rider <joshuaadams18@gmail.com>
2017-05-06 22:03:34 -0400

    added showcase for assets, reorganized file structure

---
+++ Assets/Weapons/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float projectileSpeed;
+    float damageCaused;
+    
+    public void SetDamage(float damage)
+    {
+        damageCaused = damage;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Component damageableComponent = collision.gameObject.GetComponent(typeof(IDamageable));
+
+        if(damageableComponent)
+        {
+            (damageableComponent as IDamageable).TakeDamage(damageCaused);
+        }
+        Destroy(gameObject, 0.05f);
+    }
+}
